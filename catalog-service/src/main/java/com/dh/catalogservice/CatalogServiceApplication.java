57178940526732d8c4fd7c67afb075ea186c10d1
Tomas <tomasaugustoarrue@gmail.com>
2023-08-20 18:46:04 -0300

    Primer commit del proyecto, se agrego Eureka Server y los micro servicios Movie-Service y Catalog-Service estan reconocidos. Adicionalmente se agrego .gitignore para omitir la carpeta .idea que crea IntelliJ IDEA

--- catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/dh/catalogservice/CatalogServiceApplication.java
@@ -2,9 +2,11 @@ package com.dh.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
--- movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
+++ movie-service/src/main/java/com/dh/movieservice/MovieServiceApplication.java
@@ -5,11 +5,13 @@ import com.dh.movieservice.repository.MovieRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableEurekaClient
 public class MovieServiceApplication {
 
     public static void main(String[] args) {
